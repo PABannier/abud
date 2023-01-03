a070e8246ebfa293b898802c457f78cc72097cfa
Pierre-Antoine Bannier <pierreantoine.bannier@gmail.com>
2023-01-03 08:10:30 +0100

    pubsub as objects

--- publisher.py
+++ publisher.py
@@ -3,30 +3,46 @@ import uuid
 from utils import write_data
 
 
+class Publisher:
+    def __init__(self, host: str, port: int):
+        self.host = host
+        self.port = port
+        self.id = uuid.uuid4().hex[:8]
+
+    async def connect(self):
+        try:
+            _, self.writer = await asyncio.open_connection(
+                host=self.host, port=self.port)
+            # subscribing to /null since we are publisher
+            await write_data(self.writer, b'/null')
+            print(f"Starting up {self.writer.get_extra_info('sockname')} ({self.id})")
+        except ConnectionRefusedError as exception:
+            print(f"Could not connected to server: {exception}")
+
+    async def close(self):
+        self.writer.close()
+        await self.writer.wait_closed()
+
+    async def publish(self, message: str, channel: str):
+        bytes_channel = channel.encode()
+        bytes_message = message.encode()
+
+        try:
+            await write_data(self.writer, bytes_channel)
+            await write_data(self.writer, bytes_message)
+        except OSError:
+            print("Connection ended.")
+        except asyncio.CancelledError:
+            self.close()
+
+
 async def main(host: str, port: int, channel: str):
-    me = uuid.uuid4().hex[:8]
-    print(f"Starting up {me}")
-    _, writer = await asyncio.open_connection(host=host, port=port)
-    print(f"I am {writer.get_extra_info('sockname')}")
-
-    # subscribing to /null since we are publisher
-    await write_data(writer, b'/null')
-
-    chan = channel.encode()
-
-    try:
-        for i in range(100):
-            await asyncio.sleep(1)
-            message = str(i).encode()
-            try:
-                await write_data(writer, chan)
-                await write_data(writer, message)
-            except OSError:
-                print("Connection ended.")
-                break
-    except asyncio.CancelledError:
-        writer.close()
-        await writer.wait_closed()
+    publisher = Publisher(host, port)
+    await publisher.connect()
+    for i in range(3):
+        await asyncio.sleep(1)
+        await publisher.publish(str(i), channel)
+    await publisher.close()
 
 
 if __name__ == "__main__":
--- subscriber.py
+++ subscriber.py
@@ -3,28 +3,42 @@ import uuid
 from utils import read_data, write_data
 
 
-async def client(host: str, port: int, channel: str):
-    me = uuid.uuid4().hex[:8]
-    print(f"Starting up {me}")
-    reader, writer = await asyncio.open_connection(host, port)
+class Subscriber:
+    def __init__(self, host: str, port: int):
+        self.host = host
+        self.port = port
+        self.id = uuid.uuid4().hex[:8]
 
-    # subscribing to `channel`
-    chan = channel.encode()
-    await write_data(writer, chan)
+    async def connect(self, channel: str):
+        try:
+            self.reader, self.writer = await asyncio.open_connection(self.host, self.port)
+            chan = channel.encode()
+            await write_data(self.writer, chan)  # subscribing to `channel`
+            print(f"Starting up {self.writer.get_extra_info('sockname')} ({self.id})")
+            print(f"Subscribed to {channel}.")
+        except ConnectionRefusedError as exception:
+            print(f"Could not connected to server: {exception}")
 
-    try:
-        while data := await read_data(reader):
-            print(f"Received by {me}: {data[:20]}")
-        print("Connection ended.")
-    except asyncio.IncompleteReadError:
-        print("Server closed.")
-    finally:
-        writer.close()
-        await writer.wait_closed()
+    async def listen(self):
+        try:
+            while data := await read_data(self.reader):
+                print(f"Received: {data[:20]}")
+            print("Connection ended.")
+        except asyncio.IncompleteReadError:
+            print("Server closed.")
+        finally:
+            self.writer.close()
+            await self.writer.wait_closed()
+
+
+async def main(host: str, port: int, channel: str):
+    subscriber = Subscriber(host, port)
+    await subscriber.connect(channel)
+    await subscriber.listen()
 
 
 if __name__ == "__main__":
     try:
-        asyncio.run(client("127.0.0.1", 8000, "/general"))
+        asyncio.run(main("127.0.0.1", 8000, "/general"))
     except KeyboardInterrupt:
         print("Bye!")
