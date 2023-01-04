265d22e9906b97c800d3e06a0a2ce72360e53e30
Pierre-Antoine Bannier <pierreantoine.bannier@gmail.com>
2023-01-04 08:58:24 +0100

    added docstring for utils

--- abud/utils.py
+++ abud/utils.py
@@ -2,6 +2,18 @@ from asyncio import StreamReader, StreamWriter
 
 
 async def read_data(stream: StreamReader) -> bytes:
+    """Read data from a stream by reading the size of the payload then the payload.
+
+    Arguments
+    ---------
+    stream : StreamReader
+        Read open socket to stream.
+
+    Returns
+    -------
+    data : bytes
+        Bytes of received data.
+    """
     size_bytes = await stream.readexactly(4)
     size = int.from_bytes(size_bytes, byteorder="big")
     data = await stream.readexactly(size)
@@ -9,6 +21,19 @@ async def read_data(stream: StreamReader) -> bytes:
 
 
 async def write_data(stream: StreamWriter, data: bytes):
+    """Write data to a stream.
+
+    The data is sent using the following format: the 4 first bytes correspond to the
+    size of the payload in bytes, the subsequent bytes represent the actual payload.
+
+    Arguments
+    ---------
+    stream : StreamWriter
+        Write open socket to stream.
+
+    data : bytes
+        Bytes of data to send.
+    """
     size_bytes = len(data).to_bytes(4, byteorder="big")
     stream.writelines([size_bytes, data])
     await stream.drain()
